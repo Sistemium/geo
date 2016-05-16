e425ff48e82a2c8a24b52fa04f85ac34c4e49bea
Albert <kovalevskij.albert@gmail.com>
2016-05-16 17:06:09 +0300

    geo sql

---
+++ sql/init-geo.sql
@@ -0,0 +1,18 @@
+grant connect to prt;
+grant dba to prt;
+
+util.setUserOption 'asamium.default.domain', 'geo';
+
+meta.defineType 'direction';
+meta.defineType 'distance:INTEGER';
+meta.defineType 'duration:INTEGER';
+meta.defineType 'startLocationLat:FLOAT';
+meta.defineType 'startLocationLng:FLOAT';
+meta.defineType 'endLocationLat:FLOAT';
+meta.defineType 'endLocationLng:FLOAT';
+
+meta.defineEntity 'Direction',
+ 'direction;distance;duration;startLocationLat;startLocationLng;endLocationLat;endLocationLng'
+;
+
+meta.createTable 'Direction',0,1;
